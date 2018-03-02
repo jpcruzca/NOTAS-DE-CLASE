6d5b08e9189d8b3417724228df8592544b7444ab
JUAN PABLO NICOLAS CRUZ CASTIBLANCO <jpcruzca@darkstar.org>
2018-03-02 08:54:46 -0500

    gfgbfgbfgbfgbfb
    ,kjkjmjkhmjh
    l,kjkjkm
    ,kj,kj

---
+++ CLASE2/Peso.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+  int w=0;
+
+  std::cout << "¿cual es tu edad?\n";
+
+  std::cin >> w;
+
+  std::cout << " tu edad  es : "<< w <<" años\n";
+
+  return 0;
+}
+  
